1038ce6fd6e09bdd4e023aa060c9847c3f2d8de4
Chris Gyurgyik <gyurgyikcp@gmail.com>
2020-01-10 13:43:35 -0500

    Test suite structure.

--- STL_examples.cpp
+++ STL_examples.cpp
@@ -1,8 +1,41 @@
 #include "gtest/gtest.h"
-#include <string>
+#include <algorithm>
+#include <iterator>
 
 // Examples for each STL algorithm. Verified with Google test suite.
 
-TEST(Algorithm1, Test1) {
-    EXPECT_EQ(0, 0);
-}
+// Non-modifying sequence operations.
+TEST(all_of, ExampleOne) {}
+
+TEST(any_of, ExampleOne) {}
+
+TEST(none_of, ExampleOne) {}
+
+TEST(for_each, ExampleOne) {}
+
+TEST(for_each_n, ExampleOne) {}
+
+TEST(count, ExampleOne) {}
+
+TEST(count_if, ExampleOne) {}
+
+TEST(mismatch, ExampleOne) {}
+
+TEST(find, ExampleOne) {}
+
+TEST(find_if, ExampleOne) {}
+
+TEST(find_if_not, ExampleOne) {}
+
+TEST(find_end, ExampleOne) {}
+
+TEST(find_first_of, ExampleOne) {}
+
+TEST(adjacent_find, ExampleOne) {}
+
+TEST(search, ExampleOne) {}
+
+TEST(search_n, ExampleOne) {}
+
+// Modifying sequence operations.
+
