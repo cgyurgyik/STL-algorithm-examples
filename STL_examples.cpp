2a2f35f14a6436c316faeb5e2447a74a8130a708
Chris Gyurgyik <gyurgyikcp@gmail.com>
2020-01-10 14:03:57 -0500

    any_of, all_of

--- STL_examples.cpp
+++ STL_examples.cpp
@@ -2,12 +2,46 @@
 #include <algorithm>
 #include <iterator>
 
-// Examples for each STL algorithm. Verified with Google test suite.
+// Examples for each STL algorithm. Verified with Google test suite. While code duplication is rampant,
+// The goal is to provide self-fulfilling examples that can be pulled individually
+// for others to use and look at.
 
 // Non-modifying sequence operations.
-TEST(all_of, ExampleOne) {}
+TEST(any_of, ExampleOne) {
+    const std::vector<int> numbers{1,2,3,4,4,5};
+    const bool equals_four = std::any_of(numbers.cbegin(), numbers.cend(), [](int i)->int{ return i == 4; });
+    EXPECT_TRUE(equals_four);
+    const bool equals_zero = std::any_of(numbers.cbegin(), numbers.cend(), [](int i)->int{ return i == 0; });
+    EXPECT_FALSE(equals_zero);
+}
 
-TEST(any_of, ExampleOne) {}
+TEST(any_of, ExampleTwo) {
+    // Given the empty set, std::any_of will return false.
+    const std::vector<int> numbers;
+    const bool equals_four = std::any_of(numbers.cbegin(), numbers.cend(), [](int i)->int{ return i == 4; });
+    EXPECT_FALSE(equals_four);
+    const bool equals_zero = std::any_of(numbers.cbegin(), numbers.cend(), [](int i)->int{ return i == 10; });
+    EXPECT_FALSE(equals_zero);
+}
+
+TEST(all_of, ExampleOne) {
+    const std::vector<int> all_ones{1,1,1,1};
+    const bool equals_one = std::all_of(all_ones.cbegin(), all_ones.cend(), [](int i)->int{ return i == 1; });
+    EXPECT_TRUE(equals_one);
+
+    const std::vector<int> not_all_ones{1,1,1,1,1,2};
+    const bool last_member_equals_one = std::all_of(not_all_ones.cbegin(), not_all_ones.cend(), [](int i)->int{ return i == 1; });
+    EXPECT_FALSE(last_member_equals_one);
+}
+
+TEST(all_of, ExampleTwo) {
+    // Given the empty set, std::all_of will return true.
+    const std::vector<int> numbers;
+    const bool equals_four = std::all_of(numbers.cbegin(), numbers.cend(), [](int i)->int{ return i == 4; });
+    EXPECT_TRUE(equals_four);
+    const bool equals_zero = std::all_of(numbers.cbegin(), numbers.cend(), [](int i)->int{ return i == 10; });
+    EXPECT_TRUE(equals_zero);
+}
 
 TEST(none_of, ExampleOne) {}
 
@@ -38,4 +72,26 @@ TEST(search, ExampleOne) {}
 TEST(search_n, ExampleOne) {}
 
 // Modifying sequence operations.
+
+// Partitioning operations.
+
+// Sorting operations.
+
+// Binary search operations (on sorted ranges).
+
+// Other operations (on sorted ranges).
+
+// Set operations (on sorted ranges).
+
+// Heap operations.
+
+// Minimum, maximum operations.
+
+// Comparison operations.
+
+// Permutation operations.
+
+// Numeric operations.
+
+// Operations on uninitialized memory.
 
