2cd353ac186e267392be25c5c9222275c56f1131
Chris Gyurgyik <gyurgyikcp@gmail.com>
2020-01-16 19:10:18 -0500

    Lexicographical compare.

--- STL_examples.cpp
+++ STL_examples.cpp
@@ -917,7 +917,6 @@ TEST(equal, ExampleOne) {
     const std::vector<int> v1{1,2,3,4,5};
     const std::vector<int> v2{1,2,3,4,5};
     const bool is_equal1 = std::equal(v1.cbegin(), v1.cend(), v2.cbegin());
-
     EXPECT_TRUE(is_equal1);
 
     const std::vector<int> diff{-1,2,3,4,5};
@@ -925,7 +924,12 @@ TEST(equal, ExampleOne) {
     EXPECT_FALSE(is_equal2);
 }
 
-TEST(lexicographical_compare, ExampleOne) {}
+TEST(lexicographical_compare, ExampleOne) {
+    const std::vector<char> v1 {'a', 'p', 'e'};
+    const std::vector<char> v2 {'a', 'p', 'p', 'l', 'e'};
+    const bool lex = std::lexicographical_compare(v1.cbegin(), v1.cend(), v2.cbegin(), v2.cend());
+    EXPECT_TRUE(lex);
+}
 
 TEST(lexicographical_compare_three_way, ExampleOne) {}
 
